5ec1b94980c33f1b6d5aea0460a0c8d2f9db8191
Patrick Basamot <patrickbasamot@gmail.com>
2022-06-11 07:35:54 +0800

    added a product name component

--- saver-react/src/Components/Product.js
+++ saver-react/src/Components/Product.js
@@ -1,19 +1,31 @@
-import React, {useEffect} from "react";
+import React, {useEffect,useState} from "react";
 
 function Product(props){
-    async function FetchData(){
-        const url = 'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'
-       const response = await fetch(url);
-       const data = await response.json();
-        console.log(data)
-        }   
+
+   
+    const [products, setProducts] = useState([]);
  useEffect(()=>{
      FetchData();
  })   
+
+ async function FetchData(){
+    const url = 'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'
+   const response = await fetch(url);
+   const data = await response.json();
+    console.log(data)
+    setProducts(data)
+    }   
     return(
         <div>
         <h1>Product Component,{props.productName}</h1>
+  
 
+
+       <ul>
+        {products.map((item, i) => {
+          return <li key={i}>{item.name}</li>
+        })}
+      </ul>
         </div>
     );
 }
